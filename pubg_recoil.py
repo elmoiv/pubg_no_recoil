9a9d38a49ee0c24183d137639b271ed1b79e2287
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2020-06-03 20:12:45 +0200

    Add files via upload

---
+++ pubg_recoil.py
@@ -0,0 +1,28 @@
+import pyautogui as pg
+import time, win32api
+import random
+
+def no_recoil(factor):
+    base_state = win32api.GetKeyState(0x01)
+    now = 0
+
+    while True:
+        try:
+            current_state = win32api.GetKeyState(0x01)
+            
+            if current_state != base_state:
+                base_state = current_state
+                if current_state < 0:
+                    now = 1
+                else:
+                    now = 0
+            print('\r' + ['Idle     ', 'Shooting!'][now], end='\r')
+            time.sleep(0.001)
+            if now:
+                x, y = pg.position()
+                pg.moveTo(x, y + factor, duration=0)
+        except KeyboardInterrupt:
+            print('\nNo Recoil terminated!')
+            no_recoil(int(input('Enter New Factor >>> ')))
+
+no_recoil(int(input('Enter recoil factor: ')))
