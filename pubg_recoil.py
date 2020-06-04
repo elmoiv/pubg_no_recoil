ade5b9378bea87334aecb1fdc912672409870265
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2020-06-04 14:25:47 +0200

    Improving,...

--- pubg_recoil.py
+++ pubg_recoil.py
@@ -1,18 +1,7 @@
 import pyautogui as pg
-import time, win32api, winsound, random, os
+import time, win32api, winsound, random
 from threading import Thread
 
-def is_pressed(key_code):
-    base_state = win32api.GetKeyState(key_code)
-    while True:
-        current_state = win32api.GetKeyState(key_code)
-        
-        if current_state != base_state:
-            return True
-        time.sleep(0.001)
-
-# 0 => off
-# 1 => on
 switch, shift_y = 0, 0
 
 def no_recoil():
@@ -22,6 +11,14 @@ def no_recoil():
     now = 0
 
     while True:
+
+        # Displayed text conf
+        state_t = ['IDLE', 'SHOOTING'][now].center(10)
+        switch_t = ['OFF', 'ON '][switch].center(7)
+        shift_y_t = str(shift_y).center(6)
+        print('\r', state_t, switch_t, shift_y_t, end='\r', sep='|')
+
+        # If paused then skip
         if not switch:
             continue
         try:
@@ -33,48 +30,53 @@ def no_recoil():
                     now = 1
                 else:
                     now = 0
-            print(f"{['IDLE', 'PLAY'][now]}")
-            print(f'{switch}')
-            print(f'{shift_y}')
+
+            # Small delay to prevent eating thread
             time.sleep(0.001)
+            
+            # Shifting vs recoil
             if now:
                 x, y = pg.position()
                 pg.moveTo(x, y + shift_y, duration=0)
+        
         except KeyboardInterrupt:
             print('\nNo Recoil terminated!')
             no_recoil()
-        os.system('CLS')
 
-
-# Switcher
+# Hotkeys configurator
 def switcher():
     global switch, shift_y
 
     base_state = win32api.GetKeyState(0xC0)
     base_up = win32api.GetKeyState(0x26)
     base_down = win32api.GetKeyState(0x28)
+    
     while True:
         current_state = win32api.GetKeyState(0xC0)
         current_up = win32api.GetKeyState(0x26)
         current_down = win32api.GetKeyState(0x28)
         
+        # Pause/Resume script
         if current_state != base_state:
             base_state = current_state
-            switch = current_state
+            switch = 1 if current_state else 0                
             winsound.PlaySound('done.wav', winsound.SND_ASYNC | winsound.SND_ALIAS)
         
+        # Increase Recoil shift
         if current_up != base_up:
             base_up = current_up
-            if 0 <= shift_y < 14:
+            if -1 < shift_y < 25:
                 shift_y += 1
         
+        # Decrease Recoil shift
         if current_down != base_down:
             base_down = current_down
-            if 0 <= shift_y < 15:
+            if 0 < shift_y < 26:
                 shift_y = abs(shift_y - 1)
             
         time.sleep(0.2)
  
 if __name__ == '__main__':
+    # Threading functions to allow live customization
     Thread(target=no_recoil).start()
     Thread(target=switcher).start()
